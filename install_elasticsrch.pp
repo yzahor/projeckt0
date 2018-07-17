83f515badd88503f05a1dc3234268cedec5c0b96
yzaho <yzaho@softserveinc.com>
2018-07-17 15:07:33 +0300

    mod preinstall.sh install_elasticsrch.pp

--- install_elasticsrch.pp
+++ install_elasticsrch.pp
@@ -5,7 +5,7 @@ yumrepo { "Elastic_repository_for_6.x_packages":
   enabled => 1,
   gpgcheck => true,
   gpgkey => 'https://packages.elastic.co/GPG-KEY-elasticsearch',
-  target => '/etc/yum.repos.d/elastic-6.x.repo'
+  target => '/etc/yum.repos.d/elastic-6.x.repo',
 }  
 
 # execute 'yum update'
